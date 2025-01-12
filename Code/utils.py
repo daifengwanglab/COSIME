18362397f7ac27ed892b36191475dfedb275c93c
Jerome Choi <choi267@wisc.edu>
2025-01-12 09:09:54 -0600

    upload

--- Code/utils.py
+++
@@ -1,7 +0,0 @@
-# utils.py
-
-import logging
-
-def log_results(epoch, loss, config):
-    logging.basicConfig(filename=config['log_path'], level=logging.INFO)
-    logging.info(f"Epoch [{epoch}/{config['epochs']}], Loss: {loss.item()}")
