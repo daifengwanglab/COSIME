7d54b1641054ce337de41954c49152c9e5c95677
Jerome Choi <choi267@wisc.edu>
2025-01-15 00:44:38 -0600

    Delete code directory

--- code/model/utils.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def log_results(epoch, loss, epochs, log_path, **kwargs):
-    logging.basicConfig(filename=log_path, level=logging.INFO)
-    logging.info(f"Epoch [{epoch}/{epochs}], Loss: {loss.item()}")
