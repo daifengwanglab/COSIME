9eb2c477464cfaeaabbf02c5594abcdd28fd72e5
Jerome Choi <choi267@wisc.edu>
2025-01-20 11:45:33 -0600

    Update main.py

--- Code/Model/main.py
+++ Code/Model/main.py
@@ -107,7 +107,7 @@ def main():
 
     # Load model based on type and fusion method
     model = Model(
-        *input_dims=[input_dim_A, input_dim_B],
+        *[input_dim_A, input_dim_B],
         m_type=kwargs['task_type'],  # Use the 'type' argument as 'm_type'
         **kwargs
     )
